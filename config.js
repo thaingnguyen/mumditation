a6e42e6c0e7b292fd9acc06eb947d93af4e61043
Thai Nguyen <thainguyen@umass.edu>
2016-11-04 23:14:41 -0400

    setup oauth and http request

---
+++ config.js
@@ -0,0 +1,4 @@
+export default {
+  client_id: '227XHS',
+  client_secret: '66d742cb20fc681921ff46b2044de542'
+}
--- index.android.js
+++ index.android.js
@@ -9,22 +9,107 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  Linking
 } from 'react-native';
+import config from './config.js';
+import qs from 'qs';
+
+function getMoviesFromApiAsync() {
+  return fetch('https://facebook.github.io/react-native/movies.json')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      return responseJson.movies;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+function OAuth(client_id, cb) {
+
+   // Listen to redirection
+  Linking.addEventListener('url', handleUrl);
+  function handleUrl(event){
+    console.log(event.url);
+    Linking.removeEventListener('url', handleUrl);
+    const [, query_string] = event.url.match(/\#(.*)/);
+    console.log(query_string);
+
+    const query = qs.parse(query_string);
+    console.log(`query: ${JSON.stringify(query)}`);
+
+    cb(query.access_token);
+
+    /*if (query.state === state) {
+      cb(query.code, getProfileData, 'access_token');
+    } else {
+      console.error('Error authorizing oauth redirection');
+    }*/
+  }
+
+
+   // Call OAuth
+  const oauthurl = 'https://www.fitbit.com/oauth2/authorize?'+
+            qs.stringify({
+              client_id,
+              response_type: 'token',
+              scope: 'heartrate activity activity profile sleep',
+              redirect_uri: 'mppy://fit',
+              expires_in: '31536000',
+              //state,
+            });
+  console.log(oauthurl);
+
+  Linking.openURL(oauthurl).catch(err => console.error('Error processing linking', err));
+}
+
+function getData(access_token) {
+  fetch(
+     'https://api.fitbit.com/1/user/-/activities/heart/date/today/1d.json',
+    {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${access_token}`
+      },
+      //body: `root=auto&path=${Math.random()}`
+
+    }
+  ).then((res) => {
+    return res.json()
+  }).then((res) => {
+    console.log(`res: ${JSON.stringify(res)}`);
+  }).catch((err) => {
+    console.error('Error: ', err);
+  });
+}
 
 export default class mumditation extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movies: '',
+    };
+
+  }
+
+  componentDidMount() {
+    OAuth(config.client_id, getData);
+  }
+
   render() {
+
+    getMoviesFromApiAsync().then((movies) => {
+      this.setState({ movies: movies[0].title });
+    });
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
+          {this.state.movies}
         </Text>
       </View>
     );
