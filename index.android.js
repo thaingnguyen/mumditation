47a4d912ee2a68a16eb396fbb0fad2cf157ea00d
Justin Coon <Justin Coon>
2016-11-05 01:11:39 -0400

    added HRV calculator

--- index.android.js
+++ index.android.js
@@ -85,6 +85,91 @@ function getData(access_token) {
   });
 }
 
+function getHRV(data) {
+  var mockData = {
+    "activities-heart": [
+          {
+              "customHeartRateZones": [],
+              "dateTime": "today",
+              "heartRateZones": [
+                  {
+                      "caloriesOut": 2.3246,
+                      "max": 94,
+                      "min": 30,
+                      "minutes": 2,
+                      "name": "Out of Range"
+                  },
+                  {
+                      "caloriesOut": 0,
+                      "max": 132,
+                      "min": 94,
+                      "minutes": 0,
+                      "name": "Fat Burn"
+                  },
+                  {
+                      "caloriesOut": 0,
+                      "max": 160,
+                      "min": 132,
+                      "minutes": 0,
+                      "name": "Cardio"
+                  },
+                  {
+                      "caloriesOut": 0,
+                      "max": 220,
+                      "min": 160,
+                      "minutes": 0,
+                      "name": "Peak"
+                  }
+              ],
+              "value": "64.2"
+          }
+      ],
+      "activities-heart-intraday": {
+          "dataset": [
+              {
+                  "time": "00:00:00",
+                  "value": 64
+              },
+              {
+                  "time": "00:00:10",
+                  "value": 63
+              },
+              {
+                  "time": "00:00:20",
+                  "value": 64
+              },
+              {
+                  "time": "00:00:30",
+                  "value": 65
+              },
+              {
+                  "time": "00:00:45",
+                  "value": 65
+              }
+          ],
+          "datasetInterval": 1,
+          "datasetType": "second"
+      }
+  };
+  var arr = mockData['activities-heart-intraday']['dataset'];
+  var sum = 0;
+  for(var x = 0; x < arr.length; x++) {
+      sum += arr[x].value;
+  }
+  var mean = sum/arr.length;
+  var sumOfDifferences = 0;
+  for(var x = 0; x < arr.length; x++) {
+      sumOfDifferences += (arr[x].value-mean)*(arr[x].value-mean);
+  }
+  var HRV = Math.sqrt(sumOfDifferences/(arr.length-1));
+  return "mean: " + mean + ", HRV: " + HRV;
+}
+
+function getNumSeconds(input) {
+  var t = Date.parse(input);
+  return t.getSeconds() + (t.getMinutes()*60) + (t.getHours()*60*60);
+}
+
 export default class mumditation extends Component {
 
   constructor(props) {
@@ -121,13 +206,13 @@ export default class mumditation extends Component {
   render() {
 
     getMoviesFromApiAsync().then((movies) => {
-      this.setState({ movies: movies[0].title });
+      this.setState({ str: getHRV() });
     });
 
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          {this.state.movies}
+          {this.state.str}
         </Text>
         <Button
           style={{borderWidth: 1, borderColor: 'blue'}}
